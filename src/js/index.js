2cead3d68931512bbf90bb07a941d93482c12d49
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-08-16 00:33:45 +0900

    Set real time clock

--- src/js/index.js
+++ src/js/index.js
@@ -11,6 +11,17 @@ function getDate() {
   dateCon.innerHTML = `${days[day]} ${month}. ${date}. ${year}`;
 }
 
+function getTime() {
+  const now = new Date();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const sec = now.getSeconds();
+
+  clockCon.innerHTML = `${hour < 10 ? `0${hour}` : hour}:${
+    min < 10 ? `0${min}` : min
+  }:${sec < 10 ? `0${sec}` : sec}`;
+}
+
 function init() {
   getDate();
   getTime();
