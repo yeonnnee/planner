939f197381723afcd6a7a49069b57dbbdc24892a
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-08-16 21:28:21 +0900

    Amend background

--- src/js/background.js
+++ src/js/background.js
@@ -1,13 +1,28 @@
 const body = document.querySelector("body");
+const wrapper = document.createElement("div");
 
 function getImage() {
-  const numberOfImage = Math.floor(Math.random() * 9);
+  const numberOfImage = Math.floor(Math.random() * 3);
   const imageUrl = `src/images/${numberOfImage + 1}.jpg`;
+
   body.style.backgroundImage = `url(${imageUrl})`;
   body.style.backgroundSize = `cover`;
+  body.classList.add("showUp");
+}
+
+function loadBackground() {
+  const user = localStorage.getItem("user");
+
+  if (user === null) {
+    getImage();
+  } else {
+    body.style.backgroundColor = "#E9E5DC";
+    body.appendChild(wrapper);
+    wrapper.classList.add("wrapper");
+  }
 }
 
 function init() {
-  getImage();
+  loadBackground();
 }
 init();
