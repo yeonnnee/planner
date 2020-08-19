2fef9e20618b540cff519ce4c954e303289b3fdd
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2020-08-20 00:44:51 +0900

    get previous month and comming month

--- src/js/calender.js
+++ src/js/calender.js
@@ -1,77 +1,18 @@
-const calenderCon = document.createElement("div");
-const table = document.createElement("table");
-const tr = document.createElement("tr");
-const currentYearMonth = document.createElement("td");
-const preMonth = document.createElement("td");
-const nextMonth = document.createElement("td");
-const rightArrow = document.createElement("span");
-const leftArrow = document.createElement("span");
-
-userSection.appendChild(calenderCon);
-
-table.classList.add("calenderCon");
-tr.classList.add("calenderTitle");
-
-table.setAttribute("id", "calender-table");
-currentYearMonth.setAttribute("id", "currentYearMonth");
-
-rightArrow.setAttribute("id", "arrow");
-leftArrow.setAttribute("id", "arrow");
-
-rightArrow.innerHTML = `>`;
-leftArrow.innerHTML = `<`;
-
-calenderCon.appendChild(table);
-table.appendChild(tr);
-
-tr.appendChild(preMonth);
-tr.appendChild(currentYearMonth);
-tr.appendChild(nextMonth);
-
-preMonth.appendChild(leftArrow);
-nextMonth.appendChild(rightArrow);
-
-const dateTr = document.createElement("tr");
-const sun = document.createElement("td");
-const mon = document.createElement("td");
-const tue = document.createElement("td");
-const wed = document.createElement("td");
-const thur = document.createElement("td");
-const fri = document.createElement("td");
-const sat = document.createElement("td");
-
-sun.innerHTML = `Sun`;
-mon.innerHTML = `Mon`;
-tue.innerHTML = `Tue`;
-wed.innerHTML = `Wed`;
-thur.innerHTML = `Thur`;
-fri.innerHTML = `Fri`;
-sat.innerHTML = `Sat`;
-
-sun.style.color = "red";
-sat.style.color = "blue";
-
-table.appendChild(dateTr);
-dateTr.appendChild(sun);
-dateTr.appendChild(mon);
-dateTr.appendChild(tue);
-dateTr.appendChild(wed);
-dateTr.appendChild(thur);
-dateTr.appendChild(fri);
-dateTr.appendChild(sat);
-
-dateTr.classList.add("dayCon");
-sun.classList.add("days");
-mon.classList.add("days");
-tue.classList.add("days");
-wed.classList.add("days");
-thur.classList.add("days");
-fri.classList.add("days");
-sat.classList.add("days");
+const tDay = {
+  current: 0,
+};
 
 const today = new Date();
 const currentYear = today.getFullYear();
 const currentMonth = today.getMonth();
+
+const firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
+const lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
+const week = Math.ceil(lastDate.getDate() / 7) + 1;
+
+let firstDay = firstDate.getDay();
+
+tDay.current = today;
 const monthList = [
   "Jan",
   "Feb",
@@ -89,10 +30,21 @@ const monthList = [
 
 currentYearMonth.innerHTML = `${monthList[currentMonth]} ${currentYear}`;
 
-const firstDate = new Date(today.getFullYear(), today.getMonth(), 1);
-const lastDate = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-let firstDay = firstDate.getDay();
-const week = Math.ceil(lastDate.getDate() / 7) + 1;
+function getNextMonth() {
+  const next = new Date(tDay.current.valueOf() + 30 * 24 * 60 * 60 * 1000);
+  const nextMonth = next.getMonth();
+  const nextYear = next.getFullYear();
+  tDay.current = next;
+  currentYearMonth.innerHTML = `${monthList[nextMonth]} ${nextYear}`;
+}
+
+function getPreMonth() {
+  const pre = new Date(tDay.current.valueOf() - 30 * 24 * 60 * 60 * 1000);
+  const preMonth = pre.getMonth();
+  const preYear = pre.getFullYear();
+  tDay.current = pre;
+  currentYearMonth.innerHTML = `${monthList[preMonth]} ${preYear}`;
+}
 
 function buildCalender() {
   let leftDays = 7;
@@ -118,6 +70,8 @@ function buildCalender() {
     }
     leftDays = 7;
   }
+  leftArrow.addEventListener("click", getPreMonth);
+  rightArrow.addEventListener("click", getNextMonth);
 }
 
 function init() {
